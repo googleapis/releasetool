b632e20cff6a5a50b0c3c67d2cc5e3d303d5d02a
Chris Smith <quartzmo@gmail.com>
2018-09-25 12:37:18 -0600

    Fix Ruby start
    Re-add edit_release_notes to initialize ctx.release_notes.

--- releasetool/commands/start/ruby.py
+++ releasetool/commands/start/ruby.py
@@ -22,6 +22,7 @@ import attr
 import click
 import datetime
 import glob
+from pytz import timezone
 
 import releasetool.filehelpers
 import releasetool.git
@@ -84,6 +85,19 @@ def gather_changes(ctx: Context) -> None:
         from_=ctx.last_release_committish, to=f"{ctx.upstream_name}/master"
     )
     click.secho(f"Cool, {len(ctx.changes)} changes found.")
+
+
+def edit_release_notes(ctx: Context) -> None:
+    click.secho(f"> Opening your editor to finalize release notes.", fg="cyan")
+    release_notes = (
+        datetime.datetime.now(datetime.timezone.utc)
+        .astimezone(timezone("US/Pacific"))
+        .strftime("%m-%d-%Y %H:%M %Z\n\n")
+    )
+    release_notes += "\n".join(f"- {change}" for change in ctx.changes)
+    ctx.release_notes = releasetool.filehelpers.open_editor_with_tempfile(
+        release_notes, "release-notes.md"
+    ).strip()
 
 
 def determine_release_version(ctx: Context) -> None:
@@ -193,6 +207,7 @@ def start() -> None:
     determine_package_name(ctx)
     determine_last_release(ctx)
     gather_changes(ctx)
+    edit_release_notes(ctx)
     update_changelog(ctx)
     determine_release_version(ctx)
     create_release_branch(ctx)
