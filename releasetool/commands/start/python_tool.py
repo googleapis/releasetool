362238e4148ca656ae26082623324ec59587b630
Bu Sun Kim <busunkim@google.com>
2018-10-29 11:10:22 -0700

    Only use calver for python_tools, not python

--- releasetool/commands/start/python_tool.py
+++ releasetool/commands/start/python_tool.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import getpass
 import datetime
 
 import click
@@ -30,8 +31,25 @@ def determine_release_version(ctx: Context) -> None:
     click.secho(f"Releasing {ctx.release_version}.")
 
 
-# Python tools has different versioning, otherwise the process is the same as
-# Python libraries.
-releasetool.commands.start.python.determine_release_version = determine_release_version
+def start() -> None:
+    # Python tools use calver, otherwise the process is the same as python
+    # libraries.
+    ctx = Context()
 
-start = releasetool.commands.start.python.start
+    click.secho(f"o/ Hey, {getpass.getuser()}, let's release some stuff!", fg="magenta")
+
+    releasetool.commands.common.setup_github_context(ctx)
+    releasetool.commands.start.python.determine_package_name(ctx)
+    releasetool.commands.start.python.determine_last_release(ctx)
+    releasetool.commands.start.python.gather_changes(ctx)
+    releasetool.commands.common.edit_release_notes(ctx)
+    determine_release_version(ctx)
+    releasetool.commands.start.python.create_release_branch(ctx)
+    releasetool.commands.start.python.update_changelog(ctx)
+    releasetool.commands.start.python.update_setup_py(ctx)
+    releasetool.commands.start.python.create_release_commit(ctx)
+    releasetool.commands.start.python.push_release_branch(ctx)
+    # TODO: Confirm?
+    releasetool.commands.start.python.create_release_pr(ctx)
+
+    click.secho(f"\\o/ All done!", fg="magenta")
