a1d174b416ae466fc4efacbdaac0cbb6d5d4b0e5
Thea Flowers <theaflowers@google.com>
2018-10-31 13:55:28 -0700

    Release 2018.10.31

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 name = 'gcp-releasetool'
 description = ''
-version = '2018.10.25'
+version = '2018.10.31'
 release_status = 'Development Status :: 3 - Alpha'
 dependencies = [
     "requests",
