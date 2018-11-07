9ab8d88c9f5482f307d06c3851404d179487767e
Bu Sun Kim <busunkim@google.com>
2018-11-07 15:26:00 -0800

    Add todo about removing circle

--- releasetool/commands/tag/nodejs.py
+++ releasetool/commands/tag/nodejs.py
@@ -158,7 +158,7 @@ def tag(ctx: TagContext = None) -> TagContext:
     releasetool.commands.common.publish_via_kokoro(ctx)
 
     if ctx.interactive:
-        wait_on_circle(ctx)
+        wait_on_circle(ctx)  # TODO: Remove when releases are on Kokoro
         click.secho(f"\\o/ All done!", fg="magenta")
 
     return ctx
