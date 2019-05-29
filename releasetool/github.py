938142443ba50cf26616a96d9b79800eac7c1f2a
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2019-05-29 16:22:22 -0700

    Use magic github proxy for releasetool reporter (#166)
    * Use proxy for releasetool reporter
    
    * Remove unneeded file
    
    * Switch _GITHUB_ROOT to magic proxy when it is being used
    
    * Update to devrel-prod and add keys to release.cfg
    
    * Create separate function to search for API key.

--- releasetool/github.py
+++ releasetool/github.py
@@ -13,19 +13,51 @@
 # limitations under the License.
 
 import base64
+import os
 import re
-from typing import Sequence, Union
+from typing import List, Sequence, Union
 
 import requests
 
 
 _GITHUB_ROOT: str = "https://api.github.com"
 _GITHUB_UI_ROOT: str = "https://github.com"
+_MAGIC_GITHUB_PROXY_ROOT: str = "https://magic-github-proxy.endpoints.devrel-prod.cloud.goog"
+
+
+def _find_devrel_api_key() -> str:
+    paths: List[str] = []
+    magic_github_proxy_key: str = ""
+    if "KOKORO_KEYSTORE_DIR" in os.environ:
+        paths.append(
+            os.path.join(
+                os.environ["KOKORO_KEYSTORE_DIR"], "73713_yoshi-magic-github-proxy-key"
+            )
+        )
+
+    if "KOKORO_GFILE_DIR" in os.environ:
+        paths.append(
+            os.path.join(
+                os.environ["KOKORO_GFILE_DIR"], "yoshi-magic-github-proxy-key.txt"
+            )
+        )
+
+    for path in paths:
+        if os.path.exists(path):
+            with open(path, "r", encoding="utf-8") as fh:
+                magic_github_proxy_key = fh.read().strip()
+                break
+
+    if magic_github_proxy_key == "":
+        raise Exception("A magic github proxy api key is required.")
+
+    return magic_github_proxy_key
 
 
 class GitHub:
-    def __init__(self, token: str) -> None:
+    def __init__(self, token: str, use_proxy: bool = False) -> None:
         self.session: requests.Session = requests.Session()
+        self.GITHUB_ROOT = _GITHUB_ROOT
         self.session.headers.update(
             {
                 "Accept": "application/vnd.github.v3+json",
@@ -33,10 +65,15 @@ class GitHub:
             }
         )
 
+        if use_proxy:
+            self.GITHUB_ROOT = _MAGIC_GITHUB_PROXY_ROOT
+            # To use the proxy, we need an api key for the magic github proxy.
+            self.session.params = {"key": _find_devrel_api_key()}
+
     def list_pull_requests(
         self, repository: str, state: str = None, merged: bool = True
     ) -> Sequence[dict]:
-        url = f"{_GITHUB_ROOT}/repos/{repository}/pulls"
+        url = f"{self.GITHUB_ROOT}/repos/{repository}/pulls"
         response = self.session.get(url, params={"state": state})
         response.raise_for_status()
 
@@ -46,7 +83,7 @@ class GitHub:
         return response.json()
 
     def get_pull_request(self, repository: str, number: str) -> dict:
-        url = f"{_GITHUB_ROOT}/repos/{repository}/pulls/{number}"
+        url = f"{self.GITHUB_ROOT}/repos/{repository}/pulls/{number}"
         response = self.session.get(url)
         response.raise_for_status()
         return response.json()
@@ -54,7 +91,7 @@ class GitHub:
     def create_pull_request(
         self, repository: str, head: str, title: str, body: str = None
     ) -> dict:
-        url = f"{_GITHUB_ROOT}/repos/{repository}/pulls"
+        url = f"{self.GITHUB_ROOT}/repos/{repository}/pulls"
         response = self.session.post(
             url,
             json={
@@ -75,7 +112,7 @@ class GitHub:
         return re.sub(match, replacement, text)
 
     def get_contents(self, repository: str, path: str, ref: str = None) -> bytes:
-        url = f"{_GITHUB_ROOT}/repos/{repository}/contents/{path}"
+        url = f"{self.GITHUB_ROOT}/repos/{repository}/contents/{path}"
         response = self.session.get(url, params={"ref": ref})
         response.raise_for_status()
         return base64.b64decode(response.json()["content"])
@@ -88,7 +125,7 @@ class GitHub:
         name: str,
         body: str,
     ) -> dict:
-        url = f"{_GITHUB_ROOT}/repos/{repository}/releases"
+        url = f"{self.GITHUB_ROOT}/repos/{repository}/releases"
         response = self.session.post(
             url,
             json={
@@ -104,7 +141,7 @@ class GitHub:
     def add_issue_labels(
         self, repository: str, issue_number: str, labels: Sequence[str]
     ) -> dict:
-        url = f"{_GITHUB_ROOT}/repos/{repository}/issues/{issue_number}/labels"
+        url = f"{self.GITHUB_ROOT}/repos/{repository}/issues/{issue_number}/labels"
         response = self.session.post(url, json={"labels": labels})
         response.raise_for_status()
         return response.json()
@@ -112,7 +149,7 @@ class GitHub:
     def replace_issue_labels(
         self, owner: str, repository: str, number: str, labels: Sequence[str]
     ) -> dict:
-        url = f"{_GITHUB_ROOT}/repos/{owner}/{repository}/issues/{number}/labels"
+        url = f"{self.GITHUB_ROOT}/repos/{owner}/{repository}/issues/{number}/labels"
         response = self.session.put(url, json={"labels": labels})
         response.raise_for_status()
         return response.json()
@@ -139,20 +176,20 @@ class GitHub:
     def create_pull_request_comment(
         self, repository: str, pull_request_number: Union[str, int], comment: str
     ) -> dict:
-        repo_url = f"{_GITHUB_ROOT}/repos/{repository}"
+        repo_url = f"{self.GITHUB_ROOT}/repos/{repository}"
         url = f"{repo_url}/issues/{pull_request_number}/comments"
         response = self.session.post(url, json={"body": comment})
         response.raise_for_status()
         return response.json()
 
     def get_release(self, repository: str, tag_name: str) -> dict:
-        url = f"{_GITHUB_ROOT}/repos/{repository}/releases/tags/{tag_name}"
+        url = f"{self.GITHUB_ROOT}/repos/{repository}/releases/tags/{tag_name}"
         response = self.session.get(url)
         response.raise_for_status()
         return response.json()
 
     def get_tag_sha(self, repository: str, tag_name: str) -> str:
-        url = f"{_GITHUB_ROOT}/repos/{repository}/tags"
+        url = f"{self.GITHUB_ROOT}/repos/{repository}/tags"
         response = self.session.get(url)
         response.raise_for_status()
 
