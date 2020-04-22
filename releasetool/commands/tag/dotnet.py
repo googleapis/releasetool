d66196159b73e59c65eb6acf2809b049e5b56b13
Jon Skeet <jonskeet@google.com>
2020-04-22 09:15:56 +0100

    feat: Improve the .NET GitHub release format
    - Our releases now have titles of just "Foo version Bar"
    - Copy the body of the PR as the body of the release
    
    Note that the current .NET TagReleases code performs reformatting
    from the commit message, so that multiple lines are "unwrapped" into
    single long lines, etc. We can move that functionality into
    ReleaseManager in a new "push" command that will create the PR (with
    an autorelease label), so the PR body should already be fine for the
    release.

--- releasetool/commands/tag/dotnet.py
+++ releasetool/commands/tag/dotnet.py
@@ -60,8 +60,12 @@ def create_releases(ctx: TagContext) -> None:
                 repository=ctx.upstream_repo,
                 tag_name=tag,
                 target_commitish=commitish,
-                name=tag,
-                body=f"Package {package} version {version}",
+                name=f"{package} version {version}",
+                # TODO: either reformat the message as we do in TagReleases,
+                # or make sure we create the PR with an "already-formatted"
+                # body. (The latter is probably simpler, and will make the
+                # PR easier to read anyway.)
+                body=ctx.release_pr["body"],
                 # Versions like "1.0.0-beta01" or "0.9.0" are prerelease
                 prerelease="-" in version or version.startswith("0."),
             )
