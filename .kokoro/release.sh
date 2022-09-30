efbaf1ce749294e97f2c818c00e69e869e272875
bencoe@google.com <bencoe@google.com>
2022-09-30 15:25:01 +0000

    chore: use requirements during all installations

--- .kokoro/release.sh
+++ .kokoro/release.sh
@@ -19,18 +19,16 @@ set -eo pipefail
 # Enable the publish build reporter
 # Note: this installs from source since we're in the releasetool repo. Other projects
 # will need to use python3 -m pip install gcp-releasetool
-python3 -m pip install github/releasetool
+python3 -m pip install github/releasetool --require-hashes -r github/releasetool/requirements.txt
+
 python3 -m releasetool publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
 
 # Move into the package, build the distribution and upload.
 cd github/releasetool
 TWINE_PASSWORD=$(cat "${KOKORO_KEYSTORE_DIR}/73713_google-cloud-pypi-token-keystore-1")
 
-# Ensure that we have the latest versions of Twine, Wheel, and Setuptools.
-python3 -m pip install --upgrade twine wheel setuptools
-
 # Disable buffering, so that the logs stream through.
 export PYTHONUNBUFFERED=1
 
 python3 setup.py sdist bdist_wheel
-twine upload --username __token__ --password "${TWINE_PASSWORD}" dist/*
+python3 -m twine upload --username __token__ --password "${TWINE_PASSWORD}" dist/*
