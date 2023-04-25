db7dba1455d96a85c294b2a894ae3d65826b9f4c
Diptanshu Mittal <43611881+diptanshumittal@users.noreply.github.com>
2023-04-25 08:59:11 -0700

    feat: Add support for triggering go release jobs (#504)
    * Added support for go tags
    
    * Added unit tests
    
    * Reformatted the files

---
+++ tests/commands/tag/test_tag_go.py
@@ -0,0 +1,25 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from releasetool.commands.tag.go import kokoro_job_name, package_name
+
+
+def test_kokoro_job_name():
+    job_name = kokoro_job_name("upstream-owner/upstream-repo", "some-package-name")
+    assert job_name == "cloud-devrel/client-libraries/go/upstream-repo/release"
+
+
+def test_package_name():
+    name = package_name({"head": {"ref": "release-storage-v1.2.3"}})
+    assert name is None
