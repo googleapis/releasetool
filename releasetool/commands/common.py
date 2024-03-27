20bbb56aa16ac3a4ebff11f618d40ca12f11c5b1
Jeff Ching <chingor@google.com>
2024-03-27 13:09:09 -0700

    Update common.py

--- releasetool/commands/common.py
+++ releasetool/commands/common.py
@@ -169,11 +169,8 @@ def release_exists(ctx: TagContext) -> bool:
 
 
 def publish_via_kokoro(ctx: TagContext) -> None:
-    kokoro_url = "https://fusion2.corp.google.com/ci;prev=s/kokoro/"
-
-    ctx.fusion_url = parse.urljoin(
-        kokoro_url, parse.quote_plus(f"prod:{ctx.kokoro_job_name}")
-    )
+    kokoro_url = "https://fusion2.corp.google.com/ci;prev=s/kokoro/prod"
+    ctx.fusion_url = f"${kokoro_url}:${parse.quote_plus(ctx.kokoro_job_name)}"
 
     if ctx.interactive:
         pyperclip.copy(ctx.release_tag)
